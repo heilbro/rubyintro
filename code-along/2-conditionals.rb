e7b1948c6db4e886d23db11f3ef1f52ca77083ba
Michael Heilbronner <michaelheilbronner2025@u.northwestern.edu>
2024-04-19 14:24:53 +0000

    pacticing

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -2,14 +2,53 @@
 # is the same as where this file is located and then run:
 # ruby 2-conditionals.rb
 
+
 # Booleans
 
 # Boolean Expressions
+value1 = 3
+value2 = 2
+# puts value1 == value2
+# puts value1 == value2
+# puts value1 != value2
+# puts value1 > value2
+# puts value1 < value2
+# puts value1 <= value2
+# puts value1 >= value2
+# #end
+# if value1 == value2
+#     puts "math is broken"
+#     else
+#     puts "phew"
+# end
 
 # If Conditional Logic
+user_entered_password = 'tacos'
+real_password = 'secret'
+
+if user_entered_password == real_password
+    puts "great, you're logged in!"
+else
+    puts "wrong passord."
+end
 
 # If/Else Conditional Logic
-
+home_team = 2
+away_team = 2
+if home_team > away_team
+    "we won ! ! !"
 # Elsif Conditional Logic
 
+elsif home_team == away_team
+    puts "ok, a tie"
+else
+    puts "we lost"
+end
+
 # Combining Expressions
+temp = 81
+if temp >= 65 || temp <= 80
+    puts "it's nice"
+else
+    puts "ughhh"
+end
